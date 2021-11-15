c208f14d1868523997fb7f23247d7a7807942b00
Awtry <jorge_verah@outlook.es>
2021-11-15 15:56:41 -0600

    Segunda parte básica
    Segunda clase

--- app/src/main/java/com/awtry/todoapp/FormActivity.kt
+++ app/src/main/java/com/awtry/todoapp/FormActivity.kt
@@ -1,11 +1,107 @@
 package com.awtry.todoapp
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TimePicker
+import com.awtry.todoapp.MainActivity.Companion.NEW_TASK
+import com.awtry.todoapp.MainActivity.Companion.NEW_TASK_LLAVE
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class FormActivity : AppCompatActivity() {
+
+    private lateinit var edtTitle: EditText
+    private lateinit var edtDescription: EditText
+    private lateinit var edtDate: EditText
+    private lateinit var edtTime: EditText
+    private lateinit var btn_GuardarTarea: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        //Sección de agregar dato martillado
+        /*setResult(
+            NEW_TASK,
+            Intent().putExtra(NEW_TASK_LLAVE, Task(0, "Test", "Descripción", LocalDateTime.now()))
+        )*/
+
+        //Llamada a los campos especificos para obtener su info
+        initViews()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initViews() {
+        edtTitle = findViewById(R.id.edtTitle)
+        edtDescription = findViewById(R.id.edtDescription)
+        edtDate = findViewById(R.id.edtDate)
+        edtTime = findViewById(R.id.edtTime)
+        btn_GuardarTarea = findViewById(R.id.btn_GuardarTarea)
+
+        edtDate.setOnClickListener {
+            val nowDate = LocalDate.now()
+            DatePickerDialog(
+                this,
+                { _, year, month, day ->
+                    edtDate.setText("$day/$month/$year")
+                },
+                nowDate.year,
+                nowDate.monthValue - 1,
+                nowDate.dayOfMonth
+            ).show()
+        }
+
+        edtTime.setOnClickListener {
+            val nowTime = LocalTime.now()
+
+            TimePickerDialog(
+                this,
+                { _, hour, minute ->
+                    edtTime.setText("$hour:$minute")
+                },
+                nowTime.hour,
+                nowTime.minute,
+                true
+            ).show()
+        }
+
+        btn_GuardarTarea.setOnClickListener {
+            setResult(
+                NEW_TASK,
+                Intent().putExtra(
+                    NEW_TASK_LLAVE,
+                    Task(
+                        0,
+                        edtTitle.text.toString(),
+                        edtDescription.text.toString(),
+                        //Entregamos ya el objeto con la fecha y le damos un formato de salida.
+                        LocalDateTime.of(
+                            LocalDate.parse(
+                                edtDate.text,
+                                DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                            ),
+                            LocalTime.parse(
+                                edtTime.text,
+                                DateTimeFormatter.ofPattern("HH:mm")
+                            )
+                        )
+
+                        //edtTime.text
+                    )
+                )
+            )
+
+            //Para que ya una vez completada la recolección de datos se regrese a la vista anterior
+            finish()
+        }
     }
 }
--- app/src/main/java/com/awtry/todoapp/MainActivity.kt
+++ app/src/main/java/com/awtry/todoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.awtry.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
@@ -14,21 +15,36 @@ import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val NEW_TASK = 200
+        val NEW_TASK_LLAVE = "newTask"
+    }
+
     private lateinit var RecicleView_Task: RecyclerView
     private lateinit var btn_nuevaTarea: FloatingActionButton
 
+    private val SAVED_TASK = "task"
+
     private lateinit var adapter: TodoAdapter
 
-    private val task = mutableListOf(
+    private var task = mutableListOf(
         Task(0, "Test", "Descripción", LocalDateTime.now()),
-        Task(1, "Hola", "La cosa 2", LocalDateTime.of(2021,Month.SEPTEMBER, 7, 12,50)),
-        Task(2, "Awtry", "La cosa 3", LocalDateTime.of(2021,Month.SEPTEMBER, 21, 12,50)),
-        Task(3, "Beatrix", "Miau", LocalDateTime.of(2021,Month.DECEMBER, 21, 12,50))
+        Task(1, "Hola", "La cosa 2", LocalDateTime.of(2021, Month.SEPTEMBER, 7, 12, 50)),
+        Task(2, "Awtry", "La cosa 3", LocalDateTime.of(2021, Month.SEPTEMBER, 21, 12, 50)),
+        Task(3, "Beatrix", "Miau", LocalDateTime.of(2021, Month.DECEMBER, 21, 12, 50))
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Recargar la lista con los nuevos elementos al existir un cambio
+        savedInstanceState?.let {
+            //Al momento de pasarlo verificamos si esta vacío, si no lo esta, tomará los valores nuevos
+            //Si lo esta tomara la lista martillada
+            val savedTask = it.getParcelableArrayList<Task>(SAVED_TASK)?.toMutableList() ?: task
+            task = savedTask
+        }
 
         initViews()
         setAdapter()
@@ -39,22 +55,43 @@ class MainActivity : AppCompatActivity() {
         btn_nuevaTarea = findViewById(R.id.btn_nuevaTarea)
 
         btn_nuevaTarea.setOnClickListener {
-            adapter.add(Task(0, "Test", "Descripción", LocalDateTime.now()))
+            //adapter.add(Task(0, "Test", "Descripción", LocalDateTime.now()))
+
+            //Le pasamos toda la actividad a la otra vista
+            //startActivity(Intent(this, FormActivity::class.java))
+
+            //Se pasa la información, pero con new task ahora espera un resultado al momento de regresar
+            startActivityForResult(Intent(this, FormActivity::class.java), NEW_TASK)
         }
     }
 
     private fun setAdapter() {
         adapter = TodoAdapter(task)
 
-        RecicleView_Task.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        RecicleView_Task.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         RecicleView_Task.adapter = adapter
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
+        //Aplicamos los cambios que se hagan, agregando las tareas.
         outState.apply {
             putParcelableArrayList("task", task as ArrayList<Task>)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == NEW_TASK){
+            //Asegurar que la tarea no viene vacía para poder agregar los datos
+            data?.getParcelableExtra<Task>(NEW_TASK_LLAVE)?.let {
+                adapter.add(it)
+            }
+
+        }
+    }
+
 }
